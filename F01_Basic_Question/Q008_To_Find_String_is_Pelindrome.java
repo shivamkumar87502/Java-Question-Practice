9f5b0b49238f3e748321b19982a071bd0b9b2ea8
Shivam <shivamkumar87502@gmail.com>
2022-09-27 16:31:56 +0530

    Q008 To find the string is pallindrome

---
+++ F01_Basic_Question/Q008_To_Find_String_is_Pelindrome.java
@@ -0,0 +1,15 @@
+package F01_Basic_Question;
+
+import java.util.Scanner;
+
+public class Q008_To_Find_String_is_Pelindrome {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a word = ");
+        String str = in.nextLine();
+        
+
+    }
+    
+}
